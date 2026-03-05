b68c0f25986086275abd7d38f484fdac7d1af23f
Gidon Katten <55109236+gidonkatten@users.noreply.github.com>
2026-03-05 13:34:52 +0000

    feat: dynamically calculate guardian address (#18)

--- src/chains/avm/utils/contract.ts
+++ src/chains/avm/utils/contract.ts
@@ -1,3 +1,4 @@
+import { getApplicationAddress } from "algosdk";
 import { hexToBytes } from "viem";
 
 import { UINT64_LENGTH } from "../../../common/constants/bytes.js";
@@ -7,8 +8,9 @@ import { NttManagerWithTokenPaymentExecutorClient } from "../constants/client/nt
 import { NttManagerClient } from "../constants/client/ntt-manager.client.js";
 import { TransceiverManagerClient } from "../constants/client/transceiver-manager.client.js";
 import { WormholeTransceiverClient } from "../constants/client/wormhole-transceiver.client.js";
+import { TMPL_SIG_TEAL } from "../contracts/external/tmpl-sig.js";
 
-import type { AVMContractId } from "../../../common/types/address.js";
+import type { AVMAddress, AVMContractId } from "../../../common/types/address.js";
 import type { AlgorandClient } from "@algorandfoundation/algokit-utils";
 import type { BaseWallet as AVMSigner } from "@txnlab/use-wallet";
 
@@ -19,6 +21,22 @@ export function getVerifierSigsLogicSig(provider: AlgorandClient) {
       "base64",
     ),
   );
+}
+
+export async function getWormholeGuardianAddress(
+  provider: AlgorandClient,
+  wormholeCoreAppId: number | bigint,
+): Promise<AVMAddress> {
+  const globalState = await provider.app.getGlobalState(BigInt(wormholeCoreAppId));
+  const currentGuardianSetIndex = BigInt(globalState.currentGuardianSetIndex?.value ?? 0);
+
+  const { compiledBase64ToBytes: compiledEmitterLogicSig } = await provider.app.compileTealTemplate(TMPL_SIG_TEAL, {
+    ADDR_IDX: currentGuardianSetIndex,
+    EMITTER_ID: "guardian",
+    APP_ID: wormholeCoreAppId,
+    APP_ADDRESS: getApplicationAddress(wormholeCoreAppId).publicKey,
+  });
+  return provider.account.logicsig(compiledEmitterLogicSig).toString() as AVMAddress;
 }
 
 export function getTransceiverManagerContract(provider: AlgorandClient, appId: AVMContractId, signer?: AVMSigner) {
